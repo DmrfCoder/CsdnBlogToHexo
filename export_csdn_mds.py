2f75704205ca69dc23e98c390fcac0c086c8e04d
DmrfCoder <xuefanggang97@gmail.com>
2019-01-31 20:23:37 +0800

    update ignore

--- export_csdn_mds.py
+++ export_csdn_mds.py
@@ -11,41 +11,10 @@ import codecs
 
 
 def request_get(url):
-    UserAgent_List = [
-        "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36",
-        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2227.1 Safari/537.36",
-        "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2227.0 Safari/537.36",
-        "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2227.0 Safari/537.36",
-        "Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2226.0 Safari/537.36",
-        "Mozilla/5.0 (Windows NT 6.4; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2225.0 Safari/537.36",
-        "Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2225.0 Safari/537.36",
-        "Mozilla/5.0 (Windows NT 5.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2224.3 Safari/537.36",
-        "Mozilla/5.0 (Windows NT 10.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/40.0.2214.93 Safari/537.36",
-        "Mozilla/5.0 (Windows NT 10.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/40.0.2214.93 Safari/537.36",
-        "Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/37.0.2049.0 Safari/537.36",
-        "Mozilla/5.0 (Windows NT 4.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/37.0.2049.0 Safari/537.36",
-        "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1985.67 Safari/537.36",
-        "Mozilla/5.0 (Windows NT 5.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1985.67 Safari/537.36",
-        "Mozilla/5.0 (Windows NT 5.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.3319.102 Safari/537.36",
-        "Mozilla/5.0 (Windows NT 5.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.2309.372 Safari/537.36",
-        "Mozilla/5.0 (Windows NT 5.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.2117.157 Safari/537.36",
-        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.47 Safari/537.36",
-        "Mozilla/5.0 (Windows NT 5.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/34.0.1866.237 Safari/537.36",
-    ]
-
-    header = {
-        'User-agent': random.choice(UserAgent_List),
-        'Host': ' blog.csdn.net',
-        'Accept-Language': ' en-US,en;q=0.9,zh-CN;q=0.8,zh;q = 0.7',
-        'Referer': 'https://movie.douban.com/subject/24773958/?from=showing',
-    }
-
     session = requests.Session()
 
     headers = {
         'User-Agent': 'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2272.118 Safari/537.36'}
-    # req = urllib2.Request(url, headers=headers)
-    # html_doc = urllib2.urlopen(req).read()
     response = requests.get(url, headers=headers, timeout=3)
     return response
 
@@ -53,9 +22,9 @@ def request_get(url):
 def CrawlingItemBlog(base_url, id):
     second_url = base_url + 'article/details/'
     url = second_url + id
+    # 发送request请求并接受返回值
     item_html = request_get(url)
     if item_html.status_code == 200:
-        selector = etree.HTML(item_html.text, parser=etree.HTMLParser(encoding='utf-8'))
         '''
         需要的信息：
         1：标题
@@ -65,32 +34,37 @@ def CrawlingItemBlog(base_url, id):
         5：类别
         
         '''
-        categories = ''
-        str_tag = ''
 
+        # 利用BeautifulSoup解析返回的html
         soup = BeautifulSoup(item_html.text)
         c = soup.find(id="content_views")
-        m = html2text.html2text(c.prettify())
 
-        title = soup.find(attrs={'class': 'title-article'})
-        filename = title.get_text()
-        title = title.prettify()
+        # 标题
+        title_article = soup.find(attrs={'class': 'title-article'})
+        # 这里是将标题作为最后存储的文件名
+        file_name = title_article.get_text()
+        title_article = title_article.prettify()
 
-        str_title = 'title: ' + filename + '\n'
-        label = ''
+        # 设置hexo格式博客开头的格式（title）
+        hexo_title = 'title: ' + file_name + '\n'
 
-        # 有可能出现这篇文章没有label的情况
+        # 文章的categories
+        hexo_categories = ''
+
+        # 有可能出现这篇文章没有categories的情况
         try:
-            label = soup.find(attrs={'class': 'tags-box space'}).find(attrs={'class': 'tag-link'}).get_text()
+            hexo_categories = soup.find(attrs={'class': 'tags-box space'}).find(attrs={'class': 'tag-link'}).get_text()
         except Exception:
             pass
 
-        if label == '':
+        if hexo_categories == '':
             pass
         else:
-            label = label.replace('\t', '')
-            categories = 'categories:\n' + '- ' + label + '\n'
+            # 去除拿到的str中的'\t'
+            hexo_categories = hexo_categories.replace('\t', '')
+            hexo_categories = 'categories:\n' + '- ' + hexo_categories + '\n'
 
+        # 发表时间
         time = soup.find(attrs={'class': 'time'}).get_text()
         s_time1 = time.split('年')
         year = s_time1[0]
@@ -99,8 +73,12 @@ def CrawlingItemBlog(base_url, id):
         s_time3 = s_time2[1].split('日')
         day = s_time3[0]
         minite = s_time3[1].strip()
-        str_date = 'date: ' + year + '-' + month + '-' + day + ' ' + minite + '\n'
 
+        hexo_date = 'date: ' + year + '-' + month + '-' + day + ' ' + minite + '\n'
+
+        hexo_tags = ''
+
+        # 获取tags
         tags = ''
         try:
             tags = soup.find(attrs={'class': 'tags-box artic-tag-box'}).get_text()
@@ -112,30 +90,31 @@ def CrawlingItemBlog(base_url, id):
         else:
             tags = tags.split('\n')
             tags = tags[2]
-
             tags = tags.replace('\t', ' ')
             tags = tags.split(' ')
-            str_tag = 'tags:\n'
+            hexo_tags = 'tags:\n'
             for tag in tags:
                 if tag == '':
                     continue
                 else:
-                    str_tag = str_tag + '- ' + tag + '\n'
+                    hexo_tags = hexo_tags + '- ' + tag + '\n'
 
+        # 将html转化为markdown
         text_maker = html2text.HTML2Text()
         text_maker.bypass_tables = False
 
         text = text_maker.handle(c.prettify())
-        #有的文章名字特殊，会新建文件失败
+        # 有的文章名字特殊，会新建文件失败
         try:
-            f = codecs.open('./mds/' + filename + '.md', 'w', encoding='utf-8')
-            hexo_str = '---\n' + str_title + str_date + categories + str_tag + '\n---\n'
+            #写入文件
+            f = codecs.open('./mds/' + file_name + '.md', 'w', encoding='utf-8')
+            hexo_str = '---\n' + hexo_title + hexo_date + hexo_categories + hexo_tags + '\n---\n'
 
             f.write(hexo_str)
             f.write(text)
             f.close()
         except Exception:
-            print(filename)
+            print(file_name)
 
         return True
     else:
@@ -143,35 +122,47 @@ def CrawlingItemBlog(base_url, id):
 
 
 def start_spider():
+    # 把下面这个base_url换成你csdn的地址
     base_url = 'https://blog.csdn.net/qq_36982160/'
     second_url = base_url + 'article/list/'
+    # 从第一页开始爬取
     start_url = second_url + '1'
 
     number = 1
-    html = request_get(start_url)
     count = 0
 
+    # 开始爬取第一个article_list，返回信息在html中
+    html = request_get(start_url)
+
+    # 这个循环是对你博客的article_list页面的循环
     while html.status_code == 200:
         # 获取下一页的 url
         selector = etree.HTML(html.text)
-        number += 1
-        next_url = second_url + str(number)
-        cur_page = selector.xpath('//*[@id="mainBox"]/main/div[2]')
-        d = cur_page[0].xpath('//*[@id="mainBox"]/main/div[2]/div[2]/h4/a')
-        l = cur_page[0].findall('data-articleid')
 
-        for elem in cur_page[0]:
+        # cur_article_list_page[0]就是当前article_list页面中的文章的list
+        cur_article_list_page = selector.xpath('//*[@id="mainBox"]/main/div[2]')
+        d = cur_article_list_page[0].xpath('//*[@id="mainBox"]/main/div[2]/div[2]/h4/a')
+        l = cur_article_list_page[0].findall('data-articleid')
+
+        # 这个循环是对你每一个article_list中的那些文章的循环
+        for elem in cur_article_list_page[0]:
             item_content = elem.attrib
+            # 通过对比拿到的数据和网页中的有效数据发现返回每一个article_list中的list都有一两个多余元素，每个多余元素都有style属性，利用这一特点进行过滤
             if item_content.has_key('style'):
                 continue
             else:
                 if item_content.has_key('data-articleid'):
-                    id = item_content['data-articleid']
+                    # 拿到文章对应的articleid
+                    articleid = item_content['data-articleid']
+                    # 用于打印进度
                     count += 1
                     print(count)
+                    # 爬取单篇文章
+                    CrawlingItemBlog(base_url, articleid)
 
-                    CrawlingItemBlog(base_url, id)
-
+        # 进行下一article_list的爬取
+        number += 1
+        next_url = second_url + str(number)
         html = request_get(next_url)
 
 
